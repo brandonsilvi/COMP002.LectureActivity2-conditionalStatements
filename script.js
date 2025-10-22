c788cb604da3a701bfbc2e5720b3e7f99d9f8db2
Brandon Silvibarr <brandon.silvi@gmail.com>
2025-10-22 14:31:17 -0700

    added grade calc script

---
+++ script.js
@@ -0,0 +1,18 @@
+let myGrade = 42;
+
+if (myGrade >= 90) {
+console.log("Well done you earned an A!");
+}
+else if (myGrade >= 80) {
+console.log("You got a good work B!");
+}
+else if (myGrade >= 70) {
+console.log("You got a C!");
+}
+else if (myGrade >= 60) {
+console.log("You've got work to do because you got a D!");
+}
+else {
+console.log("You got an F, it's time for tutoring!");
+}
+
