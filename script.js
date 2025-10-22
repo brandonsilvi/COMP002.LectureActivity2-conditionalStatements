3d7203f112be1ae26a74d9e065a0e8683fc0da57
Brandon Silvibarr <brandon.silvi@gmail.com>
2025-10-22 14:47:37 -0700

    corrected the loop for odd ! outputs, added missing } and !

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ for (let i = 1; i <= 100; i++) {console.log(i);}
 //prints number 1-100
 
 for (let i = 1; i <= 100; i++) {
-if (i % 2 === 0) {
+if (i % 2 !== 0) {
 console.log("!");
 } else {console.log(i)}
+}
